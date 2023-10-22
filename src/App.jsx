4e433dd66990874cb77b9c3204e63ffa03a3fd33
DEVnitishOfficial <nitish.naroun123@gmail.com>
2023-10-22 22:07:47 +0530

    add and delete

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import AddTodo from './components/addTodo/addTodo'
+import TodoItem from './components/todoItem/todoItem'
+
+function App() {
+
+  return (
+    <>
+    <AddTodo />
+    <TodoItem />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/addTodo/AddTodo.jsx
@@ -0,0 +1,32 @@
+
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { addTodo } from '../../slices/todoSlice';
+
+function AddTodo() {
+
+const [inputText , setInputText] = useState('');
+const dispatch = useDispatch()
+
+const todoHandler = (e) => {
+    e.preventDefault();
+    dispatch(addTodo({todoText : inputText}))
+    setInputText('')
+
+}
+
+  return (
+    <form onSubmit={todoHandler} className="space-x-3 mt-12 justify-center items-center">
+        <input className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+         type="text"
+         value={inputText}
+         onChange={(e) => setInputText(e.target.value)}
+         placeholder='add your todo' />
+        <button type='submit' className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"> 
+            Add
+         </button>
+    </form>
+  )
+}
+
+export default AddTodo;
